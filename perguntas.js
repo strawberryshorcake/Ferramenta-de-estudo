de85d5e5806aeac7ebf76b07695f8fea418df36e
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2025-07-30 08:15:05 -0300

    salvar

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Gaara',
+    'Qual seu papel na vila?',
+    'Kazekage.'
+)
+criaCartao(
+    'Gaara',
+    'Qual papel de Gaara na quarta guerra ninja?',
+    'Comandante da Quarta Divisão das Forças Aliadas Shinobi,liderando batalhas e protegendo seus companheiros.'
+)
+criaCartao(
+    'Gaara',
+    'Qual a fraqueza de Gaara?',
+    'Gaara consome muito chakra,e sua areia pode se tornar pesada e difícil de controlar em contato com óleo.'
+)
