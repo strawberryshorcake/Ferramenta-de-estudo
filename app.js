2b999ab27020ea82b5b4a698fd1256ab92a84d70
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2025-08-20 08:15:47 -0300

    ajudando idosa

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+    let respostaEstaVisivel = false
+    
+    cartao.addEventListener('click', viraCartao)
+    
     container.appendChild(cartao)
 
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
 }
